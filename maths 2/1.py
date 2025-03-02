f27d3b4d8b5e26c5d4a960ef982cee383f1e1b6e
sourav upadhyay <pkrsourav@gmail.com>
2025-03-02 12:04:53 +0530

    added maths2

---
+++ maths 2/1.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+
+A = np.array([[1,2],[3,4]])
+determinant = np.linalg.det(A)
+determinant
+print(determinant)
---
+++ maths 2/3.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+
+B = np.array([[1,2,3], [4,5,6], [7,8,9]])
+determinant = np.linalg.det(B)
+determinant
+print(determinant)
+
+
+Transpose = np.transpose(B)
+print(Transpose)
